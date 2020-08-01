bdba757e9e1e6854c08eaba6a6d97a69592a58f3
Yurii Dubnytskyi <41127730+YuriiDubnytskyi@users.noreply.github.com>
2020-08-02 00:25:50 +0300

    Add files via upload

---
+++ client/build/precache-manifest.d27bbcf288debff90667a0bff31fa971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ea965720ec0ef1c98064ab31cfef6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d73403ff3522a086927",
+    "url": "/static/css/main.9f9a466e.chunk.css"
+  },
+  {
+    "revision": "8f3ea9df9bd6a01a6d79",
+    "url": "/static/js/2.2da1ac21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2da1ac21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d73403ff3522a086927",
+    "url": "/static/js/main.77bb9c4a.chunk.js"
+  },
+  {
+    "revision": "3a38fa20f7230e45f224",
+    "url": "/static/js/runtime-main.e7da96ce.js"
+  }
+]);
